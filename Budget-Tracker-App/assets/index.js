ccb57a713c274321029ee8513946dce01848f3fb
arefinMilen <arefin15-5279@diu.edu.bd>
2025-03-07 23:20:33 +0600

    practice basic of js making project(phase1,2)

---
+++ Budget-Tracker-App/assets/index.js
@@ -0,0 +1,10 @@
+// budget tracker app
+
+const userName = "Samsul Arefin";
+const income = 3000;
+const expenses = 2000;
+
+console.log("My personal budget Tracker app!!!");
+console.log('User: '+userName.toUpperCase());
+console.log('Total Income: $'+income);
+console.log('Total Expenses: $'+expenses);
