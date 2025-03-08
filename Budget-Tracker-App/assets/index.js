e9cfe2fcd6de0591c223a7b1a0c3b0ddcbe142e0
arefinMilen <arefin15-5279@diu.edu.bd>
2025-03-09 01:22:51 +0600

    using arithmetic operation phase-4

--- Budget-Tracker-App/assets/index.js
+++ Budget-Tracker-App/assets/index.js
@@ -4,7 +4,33 @@ const userName = "Samsul Arefin";
 const income = 3000;
 const expenses = 2000;
 
+// multiple expenses
+let rent = 1000;
+let groceries = 200;
+let utilities = 100;
+let internet = 50;
+
+// calculate total expenses
+let totalExpenses = rent + groceries + utilities + internet;
+
+// tax deduction(10% of income)
+let tax = income * 0.10;
+
+// net income after tax
+let netIncome = income - tax;
+
+// remaining balance
+let balance = netIncome - totalExpenses;
+
+// saving 20% of balance
+let saving = balance * 0.20;
+
 console.log("My personal budget Tracker app!!!");
-console.log('User: '+userName.toUpperCase());
+console.log('User: '+userName);
 console.log('Total Income: $'+income);
-console.log('Total Expenses: $'+expenses);
+console.log('Total Expenses: $'+totalExpenses);
+console.log('Tax Deduction (10%): $'+tax);
+console.log('Net Income After Tax: $'+netIncome);
+console.log('Remining Balance: $'+balance);
+console.log('Saving (20% of balance):$' +saving);
+console.log('Happy Saving!!!');
