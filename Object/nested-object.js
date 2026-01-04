698a3aeaee88e340b9e28b4d59e6ab25f54de5c3
arefinMilen <arefin15-5279@diu.edu.bd>
2026-01-04 23:50:31 +0600

    nested object understanding

---
+++ Object/nested-object.js
@@ -0,0 +1,14 @@
+const varsity = {
+    name: 'arefin',
+    semester:['first', 'second', 'third', 'fourth'],
+    events: ['take-off', 'unlock the algorithm', 'prompt battle', 'typing test'],
+    unique: {
+        id: 5279,
+        department: {
+            name: 'CSE',
+            building: 'Knowledge Tower'
+        }
+    }
+};
+
+console.log( varsity.unique.department.name); // Output: CSE
