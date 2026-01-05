857cc1f4a4b960e1ed3f037ad998a47b34ff7f85
arefinMilen <arefin15-5279@diu.edu.bd>
2026-01-05 16:07:52 +0600

    using keys values array of objects and property check

--- Object/nested-object.js
+++ Object/nested-object.js
@@ -12,3 +12,38 @@ const varsity = {
 };
 
 console.log( varsity.unique.department.name); // Output: CSE
+console.log( varsity.events[1]); // Output: unlock the algorithm
+const keys = Object.keys(varsity);
+console.log(keys); // Output: [ 'name', 'semester', 'events', 'unique' ]
+
+//values
+const values = Object.values(varsity);
+console.log(values);
+// Output: [ 'arefin', [ 'first', 'second', 'third', 'fourth' ], [ 'take-off', 'unlock the algorithm', 'prompt battle', 'typing test' ], { id: 5279, department: { name: 'CSE', building: 'Knowledge Tower' } } ]
+
+//check property
+const hasvarstyName = Object.keys(varsity).includes('name');
+console.log(hasvarstyName); // Output: true
+
+
+// condition in object check
+if('section' in varsity) {
+    console.log('section is present in varsity object');
+} else {
+    console.log('section is not present in varsity object');
+}
+
+// hasOwnProperty check
+if(varsity.hasOwnProperty('semester')) {
+    console.log('semester is present in varsity object');
+} else {
+    console.log('semester is not present in varsity object');
+}
+
+// property values check 
+
+if ( varsity.name === 'arefin') {
+    console.log('name is present in varsity object keys');
+} else {
+    console.log('name is not present in varsity object keys');
+}
