ec51b588058560fd605075fef4520bc639e746ae
arefinMilen <arefin15-5279@diu.edu.bd>
2026-01-17 23:59:47 +0600

    added arguments functions

---
+++ functions/arguments.js
@@ -0,0 +1,16 @@
+function addNumbers(a, b)  {
+    console.log("Arguments object:", arguments);
+    return a + b;
+}
+const sum = addNumbers(5, 10, 15, 20); // Extra argument '15' will be ignored
+console.log("Sum:", sum);
+
+
+function sumALL(a , b) {
+    let total = 0;
+    for (const num of arguments) {
+        total += num;
+
+}
+console.log(total);
+}
