2a33972e2407a5607d956c08d65d472d0b8d38f1
arefinMilen <arefin15-5279@diu.edu.bd>
2026-03-06 10:28:06 +0600

    arrays questions solved

---
+++ arrayQuestions/addAndremove.js
@@ -0,0 +1,4 @@
+let arr = ["apple", "banana", "cherry"];
+arr.push("Mango");
+arr.unshift("Strawberry");
+console.log(arr); // Output: ["Strawberry", "apple", "banana", "cherry", "Mango"]
---
+++ arrayQuestions/addProduct.js
@@ -0,0 +1,3 @@
+let arr = ["shirt", "pants", "shoes"];
+ arr.splice(1,0,"shorts","t-shirt","jins")
+console.log(arr);
